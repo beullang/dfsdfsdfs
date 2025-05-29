1175e5c4aa1b4656fabed78109b54e981e670db6
Sehwan <beullang172@gmail.com>
2025-05-29 21:58:40 +0900

    21122112
    122121212

--- TPSGameUnity-main/Assets/scripts/fire.cs
+++ TPSGameUnity-main/Assets/scripts/fire.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class fire : MonoBehaviour
 {
     public GameObject gb;
+    public Transform firepos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,9 +13,13 @@ public class fire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(gb);
+
+            var obj = Instantiate(gb, gb.transform.position, gb.transform.rotation);
+            var objrb = obj.GetComponent<Rigidbody>();
+            objrb.AddForce(new Vector3(0, 0, 100), ForceMode.Impulse);
         }
     }
 }
